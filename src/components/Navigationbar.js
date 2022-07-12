015dceb15ffe48f7ed0f9e501a69557abaadc69b
Nathanael Hermanto <nathanael887@gmail.com>
2022-07-12 23:58:32 +0200

    clean up

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -40,18 +40,11 @@ backgroundColor: alpha(theme.palette.common.white, 0.15),
   ".MuiInputBase-root": {
     width: "100%"
   }
-// width: '100%',
-// [theme.breakpoints.up('sm')]: {
-//     marginLeft: theme.spacing(1),
-//     width: 'auto',
-// },
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
 padding: theme.spacing(1, 1, 1, 1),
 height: '100%',
-// position: 'absolute',
-// pointerEvents: 'none',
 display: 'flex',
 alignItems: 'center',
 justifyContent: 'center',
@@ -61,7 +54,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 color: 'inherit',
 '& .MuiInputBase-input': {
     padding: theme.spacing(1, 0, 1, 0),
-    //paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
